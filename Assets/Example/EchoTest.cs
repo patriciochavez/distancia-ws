fce40cc2ed358f3f595341e5c2099bd1d4d18267
patriciochavez <patricio_chavez@hotmail.com>
2016-12-24 09:24:17 -0300

    first commit

---
+++ Assets/Example/EchoTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class EchoTest : MonoBehaviour {
+
+    Mensaje_Arduino mensaje_arduino;
+    public string ws_url = "ws://192.168.1.210";
+    private WebSocket w;
+    public float distancia;
+    
+    [Serializable]
+    public class Mensaje_Arduino
+    {
+        public float distancia;               
+    }
+
+    IEnumerator Start()
+    {
+        w = new WebSocket(new Uri(ws_url));
+        yield return StartCoroutine(w.Connect());
+        if (w.error != null)
+        {
+            Debug.LogError("Error: " + w.error);
+        }
+    }
+
+    void Update()
+    {
+        string recibido = w.RecvString();
+        if (JsonUtility.FromJson<Mensaje_Arduino>(recibido) != null)
+        {
+            mensaje_arduino = JsonUtility.FromJson<Mensaje_Arduino>(recibido);
+            distancia = mensaje_arduino.distancia / 10;
+            transform.position = new Vector3(0,0, -distancia);
+        }
+    }    
+}
---
+++ Assets/Scripts/Distance_WS.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Distance_WS : MonoBehaviour
+{
+
+    Mensaje_Arduino mensaje_arduino;
+    public string ws_url = "ws://192.168.1.210";
+    private WebSocket w;
+    public float distancia;
+
+    [Serializable]
+    public class Mensaje_Arduino
+    {
+        public float distancia;
+    }
+
+    IEnumerator Start()
+    {
+        w = new WebSocket(new Uri(ws_url));
+        yield return StartCoroutine(w.Connect());
+        if (w.error != null)
+        {
+            Debug.LogError("Error: " + w.error);
+        }
+    }
+
+    void Update()
+    {
+        string recibido = w.RecvString();
+        if (JsonUtility.FromJson<Mensaje_Arduino>(recibido) != null)
+        {
+            mensaje_arduino = JsonUtility.FromJson<Mensaje_Arduino>(recibido);
+            distancia = mensaje_arduino.distancia;
+            transform.position = new Vector3(0, 0, -distancia);
+        }
+    }
+}
